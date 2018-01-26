c63ef8ff11034d02601926e2c6982832f08de1fc
daniel-k-richardson <daniel@unimicro.no>
2018-01-26 08:43:56 +0100

    first

---
+++ Labs and Slides/labs/01_AuthenticationAuthorization/before/MoviesWebApp/src/MoviesWebApp/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MoviesLibrary;
+using MoviesWebApp.ViewModels;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace MoviesWebApp.Controllers
+{
+    // TODO: allow anonymous access
+    public class AccountController : Controller
+    {
+        private MovieIdentityService _identityService;
+
+        public AccountController(MovieIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            // TODO: validate password
+            // create list of claims w/ sub claim from username
+            // call into MovieIdentityService to get app specific claims and merge into claims list
+            // create claims identity and claims principal
+            // call signin to issue cookie
+            // redirect to return url, or back to home page
+
+            ModelState.AddModelError("", "Invalid username or password");
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            // TODO: remove authentication cookie by calling signout
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
---
+++ Labs and Slides/labs/01_AuthenticationAuthorization/before/MoviesWebApp/src/MoviesWebApp/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MoviesWebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMoviesLibrary();
+
+            // TODO: add authentication service to DI
+			// set the default scheme to "Cookies"
+
+			// TODO: add the cookie handler
+            // set login and access denied paths ("/Account/Login" and "/Account/AccessDenied")
+
+
+            // TODO: add authorization services to DI
+
+
+            // TODO: configure a "SearchPolicy" to only allow authenticated customers and admins
+
+            // TODO: register the authorization handlers in DI
+
+			
+            // Add framework services.
+            services.AddMvc(options =>
+            {
+                // TODO: add a AuthorizeFilter that uses a policy that prevents anonymous access
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            // TODO: add the authentication middleware
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
